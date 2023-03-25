7cfe3b5d6bdbdec6f5ac2f22a8f925eeeed06efe
VISHAL MISHRA <beyondinfinity9988@gmail.com>
2023-03-25 02:30:59 -0700

    gfds

---
+++ components/Footer.jsx
@@ -0,0 +1,45 @@
+import React from "react";
+import Image from 'next/image';
+import SupportChatGif2 from '../public/images/SupportChatGif2.gif'
+export default function Footer() {
+    return (
+        <section className="py-5 bg-black">
+
+            <div className="w-[200] bg-black flex justify-between ">
+                <Image src={SupportChatGif2} alt="my gif" height={400} width={400} />
+                <div className="bg-black font-sans text-right w-[200] pr-20 pt-20 flex">
+                    <h1 className="text-white text-5xl w-[-2/3] font-bold ">Our support team is <span className="text-orange-500 &nbsp">active 24 x 7 <p className="text-gray-600 text-xl pt-10"> Feel free to chat with our support team whenever you need more clarity </p></span></h1>
+                </div>
+            </div>
+
+            <div className=" bg-black text-center font-kanit font-extrabold ">
+                <h1 className="text-5xl text-white  "> Over <span className="text-orange-500"> 10 million </span>happy stories</h1>
+
+            </div>
+        </section>
+
+
+
+
+    )
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
--- pages/index.js
+++ pages/index.js
@@ -2,7 +2,7 @@ import Head from 'next/head'
 import Image from 'next/image'
 import Navbar from '@/components/Navbar'
 import Hero from '@/components/Hero'
-
+import Footer from '@/components/Footer'
 
 export default function Home() {
   return (
@@ -16,6 +16,7 @@ export default function Home() {
       <main className=''>
         <Navbar/>
         <Hero/>
+        <Footer/>
       </main>
       </>
   )
